7a5fe1989d7cc501867f502ab2a5c9f9d5b0d9c8
cham_why <53082717+sjufkaus2@users.noreply.github.com>
2020-03-22 23:53:21 +0900

    create client

--- app.js
+++ app.js
@@ -8,10 +8,23 @@ const path = require('path');
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', (req, res)=>{
   res.sendFile('/index.html');
 });
+
+io.on('connection', (socket)=>{
+  socket.on('connect', ()=>{
+    console.log('a user is connected');
+  });
+  socket.on('join', (room, )=>{
+
+  });
+  socket.on('disconnect')
+});
+
+
 
 http.listen(PORT, ()=>{
   console.log(`server is open at ${PORT}`);
